15bb14e22ba19dc3b33e004227a0ccfebeeff5f3
yoshimura <yoshimura@freemind.co.jp>
2020-07-11 17:15:49 +0900

    initialize

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"./handler"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routing
+	e.GET("/main", handler.MainPage())
+
+	// Setup
+	e.Start(":8080")
+
+}
